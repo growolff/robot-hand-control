7436ce289532f84e7e6a588d3930291e5e219a6c
Gonzalo Olave Wolff <growolff@gmail.com>
2022-01-09 14:50:25 -0300

    simple struct spi

--- src/spi_mega_simple_struct/spi_mega_simple_struct.ino
+++ src/spi_mega_simple_struct/spi_mega_simple_struct.ino
@@ -1,6 +1,6 @@
 
 #include <SPI.h>
-//#include "SPI_anything.h"
+
 
 /*
    in arduino mega
@@ -10,7 +10,9 @@
 
 */
 
-#define SS_1 53 // ss for nano 1
+#define SS1 53 // ss for nano 1
+
+#define msgSize 2
 
 typedef struct fingerMsg {
   uint8_t cmd;
@@ -18,31 +20,21 @@ typedef struct fingerMsg {
 } fM;
 
 typedef union fingerMsgUnion {
-  unsigned char bytes[sizeof(fM)];
-  fM msg;
+  char bytes[msgSize];
+  fingerMsg msg;
 } fU;
 
-byte sendMsg (fingerMsg table, int ss){
+void sendMsg (fingerMsg table, int ss) {
   fingerMsgUnion msgU;
   msgU.msg = table;
-  byte a = 0; // variable to store SPI input data from Nano
 
   digitalWrite(ss, LOW);
-  for (size_t i = 0; i < sizeof(fM); i++)
+  for (size_t i = 0; i < msgSize; i++)
   {
-    a = SPI.transfer(msgU.bytes[i]);
+    SPI.transfer(msgU.bytes[i]);
   }
-  delay(1);
-  digitalWrite(ss, HIGH);
-
-  /*
-  digitalWrite(ss, LOW);
-  SPI_writeAnything(table);
-  
   delay(10);
   digitalWrite(ss, HIGH);
-  */
-  return a;
 }
 
 byte in = 0;
@@ -55,7 +47,7 @@ void setup (void)
   Serial.println ("SPI demo");
 
   // SS = slave select, built in AVR output pin reference
-  digitalWrite(SS_1, HIGH);  // ensure SS stays high for now
+  digitalWrite(SS1, HIGH);  // ensure SS stays high for now
 
   // Put SCK, MOSI, SS pins into output mode
   // also put SCK, MOSI into LOW state, and SS into HIGH state.
@@ -71,31 +63,22 @@ void setup (void)
 }  // end of setup
 
 fingerMsg msg;
-byte a = 0; // variable to store SPI input data from Nano
 
 void loop (void)
 {
-  //msg.cmd = 1; // position control
+  msg.cmd = 1; // position control
   if (Serial.available()) {
-    in = Serial.read();
+    in = Serial.parseInt();
     int aux = Serial.parseInt(); // quita el 0 que se lee siempre despues de un parseInt
     Serial.print("in: ");
-    Serial.println(in);
-    //msg.val = in;
-  }
-  
-  // get a value from the SPI - we're the master so have to ask for it from slave
-  // enable Slave Select
-  digitalWrite(SS_1, LOW);
-  a = SPI.transfer (in);
-  // disable Slave Select
-  delay(10);
-  digitalWrite(SS_1, HIGH);
-  
-  if (millis() - t_print > 100) {
-    Serial.println (a, DEC);
-    t_print = millis();
+    msg.val = in;
+    Serial.println(msg.val);
+    sendMsg(msg, SS1);
   }
 
+  if (millis() - t_print > 100) {
+    //Serial.println (a, DEC);
+    t_print = millis();
+  }
 
 }  // end of loop
