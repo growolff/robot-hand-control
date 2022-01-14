c23fe9d7612ed39710a8f486e599df0fc42334b4
Gonzalo Olave Wolff <growolff@gmail.com>
2022-01-14 00:26:56 -0300

    indice abriendo y cerrando

--- controlMano.py
+++ controlMano.py
@@ -11,43 +11,74 @@ SET_POS_REF = 1
 
 def readMotorData():
 
-  t.sleep(1)
-  mega.sendCmd(s1=SEND_DATA_TRUE)
+    t.sleep(1)
+    mega.sendCmd(s1=SEND_DATA_TRUE)
 
-  a = range(0,60)
-  for i in a:
+    a = range(0,60)
+    for i in a:
       mega.sendCmd(s1=TO_SS1,s2=0xEE)
       print(mega.getMsg())
       t.sleep(0.1)
 
-  mega.sendCmd(s1=SEND_DATA_FALSE)
-  t.sleep(0.5)
-  mega.stopProcess()
+    mega.sendCmd(s1=SEND_DATA_FALSE)
+    t.sleep(0.5)
+    mega.stopProcess()
+
+def abrirDedo():
+    print("Abrir dedo")
+    t.sleep(1)
+    mega.sendCmd(s1=SEND_DATA_TRUE)
+    t.sleep(0.1)
+
+    mmax = 110
+    mstep = 10
+    for i in range(0,mmax,mstep):
+        print(i)
+        mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=0,s4=i)
+        t.sleep(0.05)
+
+    t.sleep(4)
+
+    for i in range(0,mmax,mstep):
+        print(i)
+        mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=i,s4=mmax-10-i)
+        t.sleep(0.05)
+
+    t.sleep(5)
+
+    relax()
+
+    mega.sendCmd(s1=SEND_DATA_FALSE)
+    t.sleep(0.5)
+
+def relax():
+    mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=0,s4=0)
+    t.sleep(1)
 
 def test():
-  t.sleep(1)
-  mega.sendCmd(s1=SEND_DATA_TRUE)
-  t.sleep(0.1)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=200,s4=0)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=200,s4=0)
-  t.sleep(1)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=50)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=50)
-  t.sleep(1)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=10)
-  mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s3=10)
-  t.sleep(0.5)
-  mega.sendCmd(s1=SEND_DATA_FALSE)
-  t.sleep(0.5)
+    t.sleep(1)
+    mega.sendCmd(s1=SEND_DATA_TRUE)
+    t.sleep(0.1)
+    mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s4=100)
+    t.sleep(1)
+    mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s4=50)
+    t.sleep(1)
+    mega.sendCmd(s1=TO_SS1,s2=SET_POS_REF,s4=10)
+    t.sleep(0.5)
+    mega.sendCmd(s1=SEND_DATA_FALSE)
+    t.sleep(0.5)
 
-  mega.stopProcess()
+    mega.stopProcess()
 
 if __name__ == '__main__':
-  mega = ManoSerial('COM5',500000)
-  mega.startProcess()
-  try:
-      readMotorData()
-      #test()
-  except Exception as e:
-      print(e)
-      mega.stopProcess()
+    mega = ManoSerial('COM5',500000)
+    mega.startProcess()
+    try:
+        #readMotorData()
+        #test()
+        abrirDedo()
+
+        mega.stopProcess()
+    except Exception as e:
+        print(e)
+        mega.stopProcess()
